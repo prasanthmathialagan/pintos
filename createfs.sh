ff022c92ba85a4a1e93f68dedb50a370b70773db
Prasanth Mathialagan <pmathial@buffalo.edu>
2016-11-15 15:59:39 -0500

    script for creating filesystem and copying few examples

---
+++ createfs.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+cd /home/pintos/pintos/src/examples
+make
+cd /home/pintos/pintos/src/userprog
+make
+cd build
+echo "Creating a file system.."
+pintos-mkdisk filesys.dsk --filesys-size=2
+echo "Formatting the file system.."
+pintos -f -q
+echo "Copying examples..."
+pintos -p ../../examples/echo -a echo -- -q
+pintos -p ../../examples/pwd -a pwd -- -q
+pintos -p ../../examples/cat -a cat -- -q
+pintos -p ../../examples/cp -a cp -- -q
+pintos -p ../../examples/ls -a ls -- -q
+pintos -p ../../examples/mkdir -a mkdir -- -q
