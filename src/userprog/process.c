21b586053152d0306f2f97736e3703892f1e2b75
Prasanth Mathialagan <pmathial@buffalo.edu>
2016-11-15 16:16:03 -0500

    Added temporary code for basic working.

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -88,6 +88,8 @@ start_process (void *file_name_)
 int
 process_wait (tid_t child_tid UNUSED) 
 {
+  while(1); // FIXME: Added for initial testing
+
   return -1;
 }
 
@@ -437,7 +439,10 @@ setup_stack (void **esp)
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success)
-        *esp = PHYS_BASE;
+      {
+        // *esp = PHYS_BASE;
+        *esp = PHYS_BASE - 12; // FIXME: Added for basic working
+      }
       else
         palloc_free_page (kpage);
     }
